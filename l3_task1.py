cd446fd6d7e2f354c092e4a9a83d0d1c9453e3ea
Ekaterina Vrublevskaya <vrublevskaya.ek@yandex.ru>
2019-12-13 13:03:50 +0300

    Новая версия задания 1

--- l3_task1.py
+++ l3_task1.py
@@ -13,9 +13,4 @@ print(div(var1, var2))
     #print("%.2f" %result) - хотелось бы сделать так при выводе результата, но программа
     #ожидаемо ругнулась на строковый тип при делении на 0. 
 
-#task_2   
-def person(name, surname, year, city, e_mail, tel)
-inf = f'{name}, {surname}, {year} года рождения, {city}, {e_mail}, {tel}'
-return inf
-print(Ivan, Petrov, 1991, Dubna, i.petrov@gmail.com, 223-322)
 
